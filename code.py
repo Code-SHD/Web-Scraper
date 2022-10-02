076b685060c87ac47e60322d41afa96db3f5fa66
Code.SHD <98087876+Code-SHD@users.noreply.github.com>
2022-10-02 11:46:56 +0900

    Update code.py

--- code.py
+++ code.py
@@ -4,16 +4,16 @@ import openpyxl
 
 wb = openpyxl.Workbook()
 sheet = wb.active
-sheet.append(["value1", "value2", "value3"])
+sheet.append(["value1", "value2", "value3"]) #name of the value to be stored
 
-webpage = requests.get("")
+webpage = requests.get("") #web page link
 soup = BeautifulSoup(webpage.content, "html.parser")
-container = soup.select_one("")
+container = soup.select_one("") #the location of the tag containing the desired value
 
 for con in container:
-    value1 = container.select_one("")
-    value2 = container.select_one("")
-    value3 = container.select_one("")
+    value1 = container.select_one("") #The position of the desired value among the values in the container
+    value2 = container.select_one("") #The position of the desired value among the values in the container
+    value3 = container.select_one("") #The position of the desired value among the values in the container
     
     sheet.append([value1, value2, value3])
 
